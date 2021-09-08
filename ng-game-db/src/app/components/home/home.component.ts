6e8b32b4582d5c3d1c497cc07be3c4d653ae2a7f
kawsar-95 <nuruddinkawsar1995@gmail.com>
2021-09-08 13:09:10 +0600

    styling home done

--- ng-game-db/src/app/components/home/home.component.ts
+++ ng-game-db/src/app/components/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Params } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public sort!: string;
+  routeSub: any;
+  activatedRoute: any;
+  searchGames: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+      }
+    });
   }
-
 }
